8f0cdb0b048738c0364e89e26ae2e1ce8d015db7
DelfinaSvetliza <delfina.svetliza@gmail.com>
2024-04-16 10:16:54 -0300

    Firma

--- frmFirma.Designer.cs
+++ frmFirma.Designer.cs
@@ -28,12 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pctFirma = new System.Windows.Forms.PictureBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pctFirma)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pctFirma
+            // 
+            this.pctFirma.Location = new System.Drawing.Point(64, 22);
+            this.pctFirma.Name = "pctFirma";
+            this.pctFirma.Size = new System.Drawing.Size(227, 271);
+            this.pctFirma.TabIndex = 0;
+            this.pctFirma.TabStop = false;
+            this.pctFirma.Click += new System.EventHandler(this.pctFirma_Click);
+            this.pctFirma.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pctFirma_MouseClick);
+            this.pctFirma.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pctFirma_MouseDown);
+            this.pctFirma.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pctFirma_MouseMove);
+            this.pctFirma.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pctFirma_MouseUp);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(110, 343);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(138, 49);
+            this.btnGuardar.TabIndex = 1;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // frmFirma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(381, 434);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.pctFirma);
+            this.Name = "frmFirma";
             this.Text = "frmFirma";
+            this.Load += new System.EventHandler(this.frmFirma_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pctFirma)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pctFirma;
+        private System.Windows.Forms.Button btnGuardar;
     }
 }
--- frmFirma.cs
+++ frmFirma.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,100 @@ namespace prySvetlizaDelfina
 {
     public partial class frmFirma : Form
     {
+        private Point previousPoint;
+        private bool isMouseDrawing = false;
+        private Bitmap signatureBitmap;
+        private string fondo;
         public frmFirma()
         {
             InitializeComponent();
+            InitializeSignaturePictureBox();
+            pctFirma.MouseDown += new MouseEventHandler(pctFirma_MouseDown);
+            pctFirma.MouseMove += new MouseEventHandler(pctFirma_MouseMove);
+            pctFirma.MouseUp += new MouseEventHandler(pctFirma_MouseUp);
+        }
+        private void InitializeSignaturePictureBox()
+        {
+            signatureBitmap = new Bitmap(pctFirma.Width, pctFirma.Height);
+            pctFirma.Image = signatureBitmap;
+            Graphics g = Graphics.FromImage(pctFirma.Image);
+            g.Clear(Color.White);
+            g.Dispose();
+        }
+        private void frmFirma_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void pctFirma_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isMouseDrawing = true;
+                previousPoint = e.Location;
+            }
+        }
+
+        private void pctFirma_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void pctFirma_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDrawing)
+            {
+                using (Graphics g = Graphics.FromImage(pctFirma.Image))
+                {
+                    g.SmoothingMode = SmoothingMode.AntiAlias;
+                    using (Pen pen = new Pen(Color.Black, 2))
+                    {
+                        g.DrawLine(pen, previousPoint, e.Location);
+                    }
+                }
+                previousPoint = e.Location;
+                pctFirma.Invalidate();
+            }
+
+        }
+
+        private void pctFirma_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDrawing = false;
+        }
+
+        private void pctFirma_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            // Obtener la fecha actual
+            string fecha = DateTime.Now.ToString("yyyy_MM_dd_HHmmss") + ".png";
+
+
+            // Construir la ruta del archivo
+            string carpeta = "FIRMA";
+            string nombreArchivo = $"dibujo_{fecha}.png";
+            string rutaCompleta = Path.Combine(carpeta, nombreArchivo);
+
+            // Crear la carpeta si no existe
+            Directory.CreateDirectory(carpeta);
+
+            // Crear un bitmap del mismo tamaño que el PictureBox
+            Bitmap bmp = new Bitmap(pctFirma.Width, pctFirma.Height);
+
+            // Dibujar el contenido del PictureBox en el bitmap
+            pctFirma.DrawToBitmap(bmp, pctFirma.ClientRectangle);
+
+            // Guardar el bitmap en un archivo
+            bmp.Save(rutaCompleta);
+
+            // Liberar recursos
+            bmp.Dispose();
+
+            MessageBox.Show("Firma guardada correctamente.");
         }
     }
+    
 }
